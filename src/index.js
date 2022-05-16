9a52e50fcd5fa3ab784d4196f4c2abc35a90d4db
20b01a12i0 <20b01a12i0@svecw.edu.in>
2022-05-16 15:27:56 +0530

    thirdproject

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
+//import necessary dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Faker from 'faker';
+
+//create a component
+function Card(){
+  return(
+       <div className="ui cards">
+  <div className="card">
+    <div className="content">
+     <img className="right floated mini ui image" src={Faker.image.image()}  />
+      <div className="header">
+        Elliot Fu
+      </div>
+      <div className="meta">
+        Friends of Veronika
+      </div>
+      <div className="description">
+        Elliot requested permission to view your contact details
+      </div>
+    </div>
+    <div className="extra content">
+      <div className="ui two buttons">
+        <div className="ui basic green button">Approve</div>
+        <div className="ui basic red button">Decline</div>
+      </div>
+    </div>
+  </div>
+  <div className="card">
+    <div className="content">
+      <img className="right floated mini ui image" src={Faker.image.image()}  />
+      <div className="header">
+        Jenny Hess
+      </div>
+      <div className="meta">
+        New Member
+      </div>
+      <div className="description">
+        Jenny wants to add you to the group <b>best friends</b>
+      </div>
+    </div>
+    <div className="extra content">
+      <div className="ui two buttons">
+        <div className="ui basic green button">Approve</div>
+        <div className="ui basic red button">Decline</div>
+      </div>
+    </div>
+  </div>
+</div>
+)
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+    <Card />
+   
+    </>,
+     document.getElementById('root')
+)
